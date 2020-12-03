e49c50c442dd34725d4575a5127493f18c20e15e
Litovetc Andrii <litovetc2@gmail.com>
2020-12-03 23:05:26 +0200

    63.3 DAL refactoring

--- src/api/api.js
+++ src/api/api.js
@@ -1,39 +1,43 @@
 import * as axios from "axios";
 
+
+const BASE_URL = `https://social-network.samuraijs.com/api/1.0/`
+
 const instance = axios.create({
+    baseURL:BASE_URL,
     withCredentials: true,
     headers: {
         "API-KEY": `c233b2ab-c911-4c2a-9ca2-f04cdde337db`
     }
 });
 
-const baseURL = `https://social-network.samuraijs.com/api/1.0/`
-const baseApiKeyAuthUser = {
-    withCredentials: true,
-    headers: {
-        "API-KEY": `c233b2ab-c911-4c2a-9ca2-f04cdde337db`
-    }
-}
+//
+// const baseApiKeyAuthUser = {
+//     withCredentials: true,
+//     headers: {
+//         "API-KEY": `c233b2ab-c911-4c2a-9ca2-f04cdde337db`
+//     }
+// }
 
 // for UsersContainer
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-    return axios.get(baseURL + `users?page=${currentPage}&count=${pageSize}`,
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`,
         {withCredentials: true}
     ).then(response => response.data);
 };
 
 // for headerContainer
 export const authMe = () => {
-    return axios.get(baseURL + `/auth/me`, {withCredentials: true}).then(response => response.data);
+    return instance.get(`/auth/me`, {withCredentials: true}).then(response => response.data);
 };
 
 // for
 export const unFollowUser = (id) => {
-    return axios.delete(baseURL + `follow/${id}`, baseApiKeyAuthUser).then(response => response.data);
+    return instance.delete(`follow/${id}`).then(response => response.data);
 }
 
 
 export const followUser = (id) => {
-    return axios.post(baseURL + `follow/${id}`, null, baseApiKeyAuthUser)
+    return instance.post(`follow/${id}`, null)
         .then(response => response.data);
 }
